2760a611e59bcdbaa8f664e3583a695f058787cb
Jeevan <rakeshlalauole@gmail.com>
2025-12-24 17:49:29 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#10b981",
+        dark: "#020617"
+      }
+    }
+  },
+  plugins: []
+}
